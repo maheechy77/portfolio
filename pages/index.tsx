ba3499e937698b3e94860bcecc4241125668d5d2
maheechy77 <maheechy96@gmail.com>
2022-10-28 21:30:01 +0600

    6th push

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Home = ({ pageInfo, skills, experiences, projects, socials }: Props) => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]">
       <Head>
-        <title>{pageInfo.name} Portfolio</title>
+        <title>My Portfolio</title>
       </Head>
 
       <Header socials={socials} />
